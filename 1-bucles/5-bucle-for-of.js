6fb682d478e6f6187fdf692bfab36870311f1c7b
niedlipatrik <niedlipatrik@gmail.com>
2019-07-22 22:36:58 +0200

    Ejercicio-5

--- 1-bucles/5-bucle-for-of.js
+++ 1-bucles/5-bucle-for-of.js
@@ -33,3 +33,13 @@ let gente = [{
         edad: 10
     },
 ]
+
+var i = 0;
+while (i < gente.length) {
+  nom=gente[i].nombre;
+  if (nom.charAt(0)=="J"){
+      console.log(gente[i]);
+    };
+  i++;
+}
+
